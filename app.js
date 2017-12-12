4e9bb0969ee69ed7176692a7a4ded0f2f641ac21
AminaDado <aminadado94@gmail.com>
2017-12-12 22:06:30 +0100

    partie bdd

---
+++ app.js
@@ -0,0 +1,125 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var multer=require('multer');
+var ejs = require('ejs');
+var mysql = require('mysql');
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+const util = require('util');
+const path = require('path');
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+	user: 'root',
+	password : 'root',
+    database: 'aos'
+});
+
+app.set('view engine', 'ejs');
+
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+    destination: './ressources/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+  
+
+  
+  // Init Upload
+  const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+  }).single('myImage');
+  
+  // Check File Type
+  function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Seules les images sont autorisées!');
+    }
+  }
+// redirection vers connexion
+
+app.get('/', function (req, res) {
+     app.use(express.static(__dirname+'/ressources'));
+      res.render('connexion');
+});
+
+/*app.get('/profil', function (req, res) {
+    app.use(express.static(__dirname+'/ressources'));
+    var photos=[];
+    con.query("SELECT * FROM photo", function (err, result) {
+    if (err)
+        throw err;
+    photos=result;
+    res.render('profil', {photos});
+    });    
+});*/
+
+app.use(express.static('./ressources'));
+
+
+
+app.get('/profil', (req, res) => {
+    //var insertion= "INSERT INTO photo (image_id,"
+    con.query("SELECT * FROM photo", function(err, result){
+    if(err) throw err;
+    res.render('profil', {
+      msg: 'Image publié!',
+      images: result//`uploads/${req.file.filename}`
+    });
+  })
+});
+
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('profil', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('profil', {
+          msg: 'Error: Aucun image séléctionné!'
+        });
+      } else {
+        console.log("bonjour");
+        con.query("SELECT * FROM photo", function(err, result){
+          if(err) throw err;
+          res.render('profil', {
+            msg: 'Image publié!',
+            images: result//`uploads/${req.file.filename}`
+          });
+        })
+        
+      }
+    }
+  });
+});
+
+app.listen(8080, function () {
+    console.log('listening on port 8080!')
+})
